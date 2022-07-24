8185d19b850fedb000c2a57bba005a0f5d3ad977
kaushik7d <106391713+kaushik7d@users.noreply.github.com>
2022-07-24 18:58:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
-import pickle
 
 st.write("""
 # Subtraction of 2 given numbers
